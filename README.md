c728736bd4e2caf6f42c959d03f941662f7331f9
Cameron Moreau <cameron.cm6@gmail.com>
2016-04-16 16:20:08 -0500

    Start

---
+++ README.md
@@ -0,0 +1,10 @@
+Dispatch-React
+=====================
+
+### Usage
+
+```
+npm install
+npm start
+open http://localhost:3000
+```
---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import Test from './components/Test';
+
+ReactDOM.render(<Test />, document.getElementById('root'));
