8ed3a72293b249ef3a42db48e14de3e4b48f647c
Khanh Nguyen <khanh.nguyen@protonmail.com>
2016-04-17 11:07:39 -0500

    Added stuff

--- app/components/ReportModal.js
+++ app/components/ReportModal.js
@@ -18,7 +18,6 @@ export default class ReportModal extends Component {
   }
 
   _onRecognition(e) {
-    console.log(e.results.length);
     if(e.results[e.results.length - 1].isFinal == true) {
       let box = $('#dialog-box');
       let d = new Date,
@@ -32,24 +31,64 @@ export default class ReportModal extends Component {
       box.val(box.val() + `[${dformat}] ${e.results[e.results.length - 1][0].transcript}\n`);
 
       $.ajax({
-        url: 'https://mobihackdfw.herokuapp.com/message',
-        data: {
-          'message': e.results[e.results.length - 1][0].transcript,
-        },
-        method: 'POST',
-        success: function(response) {
-            console.log(response);
-        }
+       type: 'POST',
+       dataType: 'json',
+       url: 'https://mobihackdfw.herokuapp.com/message/',
+       data : { 'message': e.results[e.results.length - 1][0].transcript },
+       success: function(data) {
+        //  console.log(data);
+         if(data['keys'] !== undefined) {
+           for(let dat of data['keys']) {
+             if(dat == 'fire') {
+               $('#fire').addClass('selected');
+             } else if(dat == 'police') {
+               $('#police').addClass('selected');
+             } else if(dat == 'medic') {
+               $('#medic').addClass('selected');
+             }
+
+             if(dat == 'location') {
+               $('#coordinates').val(`${data['location']['latitude']}, ${data['location']['longitude']}`)
+               $('#location').val(`${data['address']}`)
+             }
+           }
+         }
+       }
       });
     }
   }
 
   _onSuccessParse(resp) {
-    console.log(resp);
+
   }
 
   close() {
     this.setState({showModal: false})
+  }
+
+  submit() {
+    let coords = $('#coordinates');
+    let type = '';
+
+    if($('#police').hasClass('selected')) {
+      type = 'police';
+    } else if($('#medic').hasClass('selected')) {
+      type = 'medic';
+    } else if($('#fire').hasClass('selected')) {
+      type = 'fire';
+    }
+
+    let status = 'reported';
+
+    $.ajax({
+     type: 'POST',
+     dataType: 'json',
+     url: 'https://mobihackdfw.herokuapp.com/report/new',
+     data : { 'type': type, 'status': status },
+     success: function(data) {
+       console.log('sent');
+     }
+    });
   }
 
   render() {
@@ -66,14 +105,18 @@ export default class ReportModal extends Component {
             <textarea id="dialog-box" className="dialog-box" name="dialog"></textarea>
           </div>
           <div className="input-group">
-            <input id="dialog-box" className="two" name="dialog" value="Location"></input>
-            <input id="dialog-box" className="two" name="dialog" value="Coordinates"></input>
+            <input id="location" className="two" name="dialog" value="Location"></input>
+            <input id="coordinates" className="two" name="dialog" value="Coordinates"></input>
           </div>
           <div className="input-group">
-            <button><img src={"/public/images/ic_police.png"} /> Police</button>
-            <button><img src={"/public/images/ic_fire_truck.png"} /> Fire</button>
-            <button><img src={"/public/images/ic_ambulance.png"} /> Medic</button>
+            <button id="police"><img src={"/public/images/ic_police.png"} /> Police</button>
+            <button id="fire"><img src={"/public/images/ic_fire_truck.png"} /> Fire</button>
+            <button id="medic"><img src={"/public/images/ic_ambulance.png"} /> Medic</button>
           </div>
+
+          <Button onClick={this.submit} bsStyle="danger" className="btn-block">
+            Submit
+          </Button>
         </Modal.Body>
       </Modal>
     </div>
